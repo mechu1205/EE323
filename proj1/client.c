85f83ce693a0dd9191cf6348e3a38af7f2ed00a7
mechu1205 <mechu1205@gmail.com>
2021-03-17 01:53:35 +0900

    update

--- proj1/client.c
+++ proj1/client.c
@@ -22,7 +22,8 @@ int main (int argc, char* argv[]){
     
     // Argument Parsing
     int input_correct = 0;
-    struct sockaddr_in host;
+    struct sockaddr_in addr_host;
+    char *host;
     uint16_t port;
     uint8_t op, shift;
     int c = 0;
@@ -30,7 +31,9 @@ int main (int argc, char* argv[]){
     while ( (c = getopt(argc, argv, "h:p:o:s:")) != -1 ){
         switch (c){
             case 'h': 
-                inet_pton(AF_INET, optarg, &host);
+                // inet_pton(AF_INET, optarg, &addr_host.sin_addr);
+                host = malloc(strlen(optarg));
+                strcpy(host, optarg);
                 input_correct ++;
                 break;
             case 'p':
@@ -55,51 +58,53 @@ int main (int argc, char* argv[]){
         exit(0);
     }
     
-    printf("port, op, shift: %d, %d, %d\n", port,op,shift);
-    fflush(stdout);
+    printf("host, port, op, shift: %s, %d, %d, %d\n", host,port,op,shift); fflush(stdout); // debug line
     
     char *buffer_in = malloc(BUFFER_SIZE);
     char *buffers_out[1];
-    char msg[MSG_SIZE+1];
+    // char msg[MSG_SIZE];
+    char *msg;
     int read_size = 0;
     uint32_t len = 0;
     int index_msg = 0;
     
     // Loop until EOF is reached
-    while(read_size = read(STDIN_FILENO, &buffer_in, BUFFER_SIZE)){
-        fprintf(stdout, "%d Bytes read\n",read_size); fflush(stdout); // debug line
-        // len = htonl(read_size + 8);
-        // bzero (msg, MSG_SIZE);
-        // memcpy(msg, &op, sizeof(op));
-        // memcpy(msg+1, &shift, sizeof(shift));
-        // memcpy(msg+4, &len, 4);
-        // memcpy(msg+8, buffer_in, read_size);
+    while(read_size = read(STDIN_FILENO, buffer_in, BUFFER_SIZE)){
+        // fprintf(stdout, "%d Bytes read\n",read_size); fflush(stdout); // debug line
+        len = htonl(read_size + 8);
+        msg = malloc(len);
+        memcpy(msg, &op, sizeof(op));
+        // fprintf(stdout, "op: %d (should be %d)\n", *((uint8_t*)msg), op); fflush(stdout); // debug line
+        memcpy(msg+1, &shift, sizeof(shift));
+        // fprintf(stdout, "shift: %d (should be %d)\n", *((uint8_t*)(msg+1)), shift); fflush(stdout); // debug line
+        memcpy(msg+4, &len, 4);
+        // fprintf(stdout, "len: %d (should be %d)\n", *((uint32_t*)(msg+4)), len); fflush(stdout); // debug line
+        memcpy(msg+8, buffer_in, read_size);
+        // fprintf(stdout, "memcpy finished\n"); fflush(stdout); // debug line
         
-        // // TODO: checksum
+        // TODO: checksum
         
-        // // Socket Creation
-        // int sockfd;
-        // struct sockaddr_in addr;
+        // Socket Creation
+        int sockfd;
         
-        // sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        // if (sockfd == -1){
-        //     fprintf(stderr, "socket creation failure: %s\n", strerror(errno));
-        //     exit(0);
-        // }
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd == -1){
+            fprintf(stderr, "socket creation failure: %s\n", strerror(errno));
+            exit(0);
+        }
         
-        // // Socket Binding
-        // bzero(&addr, sizeof(addr));
-        // addr.sin_family = AF_INET; //IPv4 protocol
-        // addr.sin_addr.s_addr = htonl(INADDR_ANY); //IP of client
-        // addr.sin_port = htons(PORT);
+        addr_host.sin_family = AF_INET; //IPv4 protocol
+        inet_pton(AF_INET, host, &addr_host.sin_addr);
+        addr_host.sin_port = port;
         
-        // if (connect(sockfd, (struct sockaddr *)&host, sizeof(host)) < 0){
-        //     fprintf(stderr, "socket connection failure: %s\n", strerror(errno));
-        //     exit(0);
-        // }else{
-        //     // Connection Established
-        //     send (sockfd, msg, );
-        // }
+        fprintf(stdout, "attempting connection..\n"); fflush(stdout); // debug line
+        if (connect(sockfd, (struct sockaddr *)&addr_host, sizeof(addr_host)) < 0){
+            fprintf(stderr, "socket connection failure: %s\n", strerror(errno));
+            exit(0);
+        }else{
+            // Connection Established
+            send (sockfd, msg, len, 0);
+        }
         
         
     }
